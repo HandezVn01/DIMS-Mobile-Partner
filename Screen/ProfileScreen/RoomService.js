ca591e254231a2282782b3feba9dcec9e04bcf06
Hoang Tuan Anh <74038883+handezVN@users.noreply.github.com>
2022-07-14 11:23:46 +0700

    1

--- Screen/ProfileScreen/RoomService.js
+++ Screen/ProfileScreen/RoomService.js
@@ -1,12 +1,47 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React, { useEffect } from 'react';
-
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 const RoomService = () => {
     return (
-        <View>
-            <Text>RoomService</Text>
+        <View style={{ flex: 1 }}>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <View style={styles.back}>
+                        <Icon name="chevron-left" size={30} color="#3DC5B5"></Icon>
+                    </View>
+                </TouchableOpacity>
+                <View style={styles.header_content}>
+                    <Text style={styles.header_title}>Room Service</Text>
+                </View>
+            </View>
+            <View style={{ flex: 17 }}></View>
         </View>
     );
 };
 
 export default RoomService;
+const styles = StyleSheet.create({
+    header: {
+        flex: 3,
+    },
+    back: {
+        height: 42,
+        width: 42,
+        borderColor: '#B2B2B2',
+        borderWidth: 1,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header_title: {
+        fontWeight: '600',
+        fontSize: 16,
+        lineHeight: 28,
+        letterSpacing: 0.5,
+        marginLeft: 15,
+    },
+    header_content: {
+        alignItems: 'center',
+    },
+});
